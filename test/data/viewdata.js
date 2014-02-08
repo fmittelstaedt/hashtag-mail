90b9c74f4f881b5403f7950ac6844cc8ca7d308c
Sam Pattuzzi <sam.pattuzzi@gmail.com>
2014-02-08 19:14:03 +0000

    Add test data.

---
+++ test/data/viewdata.js
@@ -0,0 +1,33 @@
+emails = [
+
+{
+	profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
+	from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
+	to: [
+		{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
+		{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+	],
+	datetime: "18:04 (3 min ago)",
+	body: "Great :)\n\
+Here is the readme file from there!\n\
+Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
+there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
+	hashtags: ["#readme", "#doc"],
+},
+
+{
+	profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
+	from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
+	to: [
+		{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
+		{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+	],
+	datetime: "18:04 (3 min ago)",
+	body: "Great :)\n\
+Here is the readme file from there!\n\
+Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
+there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
+	hashtags: ["#readme", "#doc"],
+}
+
+]
