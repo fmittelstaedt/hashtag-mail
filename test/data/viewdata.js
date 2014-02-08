02c73b1dd4798f821d528f8a0e260c01cdca3b0d
Ollie Marshall <ollie.marshall1@gmail.com>
2014-02-08 20:39:47 +0000

    Test data populate

--- test/data/viewdata.js
+++ test/data/viewdata.js
@@ -13,8 +13,8 @@ Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
 	hashtags: [
-		{tag: "#readme", "paragraph1 #readme"},
-		{tag: "#doc", "paragraph2 #doc"}
+		{tag: ["#readme", "paragraph1 #readme"]},
+		{tag: ["#doc", "paragraph2 #doc"]}
 	]
 },
 
@@ -31,8 +31,8 @@ Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
 	hashtags: [
-		{tag: "#readme", "paragraph3 #readme"},
-		{tag: "#doc", "paragraph4 #doc"}
+		{tag: ["#readme", "paragraph3 #readme"]},
+		{tag: ["#doc", "paragraph4 #doc"]}
 	]
 }
 
@@ -43,65 +43,65 @@ there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y
 hashtags = [
 {
 	tag: "#readme",
-	emails = [
+	emails: [
 	{
 		profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
 		from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
 		to: [
 			{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
-			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"}
 		],
 		datetime: "18:04 (3 min ago)",
 		body: "Great :)\n\
 Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
-		paragraphs: [{"paragraph1 #readme"}]
+		paragraphs: [{paragraph1: "#readme"}]
 	},
 	{
 		profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
 		from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
 		to: [
 			{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
-			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"}
 		],
 		datetime: "18:04 (3 min ago)",
 		body: "Great :)\n\
 Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
-		paragraphs: [{"paragraph3 #readme"}]
+		paragraphs: [{paragraph3: "#readme"}]
 	}]
 },
 {
 	tag: "#doc",
-	emails = [
+	emails: [
 	{
 		profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
 		from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
 		to: [
 			{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
-			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"}
 		],
 		datetime: "18:04 (3 min ago)",
 		body: "Great :)\n\
 Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
-		paragraphs: [{"paragraph2 #doc"}]
+		paragraphs: [{paragraph2: "#doc"}]
 	},
 	{
 		profile_img: "https://plus.google.com/u/0/_/focus/photos/private/AIbEiAIAAAAhCOGTkJjCoI2DpAEQ4ob1lPKJujQY1tj7xOCK6eODATAB8oRNIH1zzMb-0cGRMxXc3U540-A?sz=24",
 		from: {name:"Karina Palyutina", email:"karina.palyutina@gmail.com"},
 		to: [
 			{name:"Sam Pattuzzi", email:"sam.pattuzzi@gmail.com"},
-			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"},
+			{name:"Gregor Frédérique Mittelstaedt", email:"frederique.mittelstaedt@googlemail.com"}
 		],
 		datetime: "18:04 (3 min ago)",
 		body: "Great :)\n\
 Here is the readme file from there!\n\
 Am 08.02.2014 um 17:48 schrieb Karina <karina.palyutina@gmail.com>:\n\
 there there here https://docs.google.com/document/d/1ygvDrwJUjN0g14JbMg0Jj8rZv7y-gZ9YRmxvlj6nZVQ/edit?usp=sharing\n",
-		paragraphs: [{"paragraph4 #doc"}]
+		paragraphs: [{paragraph4: "#doc"}]
 	}]
 }];
--- ui/js/app.js
+++ ui/js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  alert(emails)
+  var wrapper = $(".list-wrapper");
+  
+  for (var i = 0; i < hashtags.length; i++) {
+    var list = $('<div class="list list-multiple well">').wrap('</div>').appendTo(wrapper);
+    
+    var list_header = $('<div class="list-header">').text(hashtags[i].tag).wrap('</div>').appendTo(list);
+    
+    var messages = $('<div class="messages">').wrap('</div>').appendTo(list);
+    
+    for (var x = 0; x < hashtags[i].emails.length; x++) {
+      var email = emails[x];
+      var message = $('<div class="message panel panel-default">').wrap('</div>').appendTo(messages);
+      var panel_heading = $('<div class="panel-heading">').html("<b>From: </b>" + email.from.email + " </br><b>To: </b>" + email.to).wrap('</div>').appendTo(message);
+      var panel_body = $('<div class="panel-body">').text(email.body).wrap('</div>').appendTo(message);
+    }
+  }
 });
