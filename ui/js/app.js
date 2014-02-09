31390fa5648f0c883fc5d37752bd59012de41e60
Ollie Marshall <ollie.marshall1@gmail.com>
2014-02-09 11:01:06 +0000

    app.js refactor

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  window.addEventListener("update_content", function(evt) {
-    console.log("should update content with.");
-    console.log(evt.hashtags);//Do something with hashtags
-  }, false);
+  //window.addEventListener("update_content", function(evt) {
+  //  console.log("should update content with.");
+  //  console.log(evt.hashtags);//Do something with hashtags
+  //}, false);
   
   var wrapper = $(".list-wrapper");
   
   for (var i = 0; i < hashtags.length; i++) {
-    
     if (hashtags.length==1) {
-           var list = $('<div class="list list-single well">').wrap('</div>').appendTo(wrapper);
-         }
-         else {
-           var list = $('<div class="list list-multiple well">').wrap('</div>').appendTo(wrapper);
-         }
+      var list = $('<div class="list list-single well">').wrap('</div>').appendTo(wrapper);
+    }
+    else {
+      var list = $('<div class="list list-multiple well">').wrap('</div>').appendTo(wrapper);
+    }
     
     var list_header = $('<div class="list-header">').text(hashtags[i].tag).wrap('</div>').appendTo(list);
     
@@ -22,24 +21,29 @@ $(document).ready(function() {
     for (var x = 0; x < hashtags[i].emails.length; x++) {
       var email = emails[x];
       var message = $('<div class="message panel panel-success">').wrap('</div>').appendTo(messages);
-      var panel_heading = $('<div class="panel-heading">').html("<b>From: </b>" + email.from.email + " </br><b>To: </b>" + email.to.length + puluralise(" recipient", email.to.length)).wrap('</div>').appendTo(message);
+      var panel_heading = $('<div class="panel-heading">').wrap('</div>').appendTo(message);
+      
+      var avatar = $('<img id="avatar" src="30.png"></img>').appendTo(panel_heading)
+      var contacts = $('<div id="contacts"></div>').appendTo(panel_heading)
+      var to = $("<b>From: </b>" + email.from.email + " </br>").appendTo(contacts)
+      contacts.append("<b>To: </b>" + email.to.length + puluralise(" recipient", email.to.length))
+      
       var panel_body = $('<div class="panel-body">').wrap('</div>').appendTo(message);
       var body1 = $('<div class="text">').text(email.body.substring(0,100)).wrap('</div>').appendTo(panel_body);
       var body2 = $('<div class="text" style="display: none;">').text(email.body).wrap('</div>').appendTo(panel_body);
-      var button = $('<button type="button" style="float:right;" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-chevron-down"></span>').wrap('</button>').appendTo(panel_body);
-
-      button.bind("click", function(e) {
-	if ($(this).find("span").hasClass("glyphicon-chevron-down")) {
-	  $(this).find("span").removeClass('glyphicon-chevron-down');
-	  $(this).find("span").addClass('glyphicon-chevron-up');
-	} else {
-	  $(this).find("span").removeClass('glyphicon-chevron-up');
-	  $(this).find("span").addClass('glyphicon-chevron-down');
-	}
-
-	$(this).parent().find(".text").toggle();
-      });
       
+      var extras = $('<div class="list-extra">').wrap('</div>').appendTo(panel_body);
+      var button = $('<button type="button" style="float:right;" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-chevron-down"></span>').wrap('</button>').appendTo(extras);
+        button.bind("click", function(e) {
+        	if ($(this).find("span").hasClass("glyphicon-chevron-down")) {
+        	  $(this).find("span").removeClass('glyphicon-chevron-down');
+        	  $(this).find("span").addClass('glyphicon-chevron-up');
+        	} else {
+        	  $(this).find("span").removeClass('glyphicon-chevron-up');
+        	  $(this).find("span").addClass('glyphicon-chevron-down');
+        	}
+	        $(this).parent().find(".text").toggle();
+      });  
     }
   }
 
