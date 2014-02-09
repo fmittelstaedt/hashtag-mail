0d95213ae265335822378e45b38264ebc3662d71
Karina Palyutina <karina.palyutina@gmail.com>
2014-02-09 10:10:06 +0000

    The toggle bug

--- ui/js/app.js
+++ ui/js/app.js
@@ -20,7 +20,13 @@ $(document).ready(function() {
       var panel_heading = $('<div class="panel-heading">').html("<b>From: </b>" + email.from.email + " </br><b>To: </b>" + email.to.length + puluralise(" recipient", email.to.length)).wrap('</div>').appendTo(message);
       var panel_body = $('<div class="panel-body">').wrap('</div>').appendTo(message);
       var body1 = $('<div class="text">').text(email.body).wrap('</div>').appendTo(panel_body);
-      var body2 = $('<div class="text" style="display: none;">').text(email.body).wrap('</div>').appendTo(panel_body);
+      var body2 = $('<div class="text" style="display: none;">').text(email.body + "3495873985793").wrap('</div>').appendTo(panel_body);
+      var button = $('<button type="button" style="float:right;" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-chevron-down"></span>').wrap('</button>').appendTo(panel_body);
+
+      button.on("click", function() {
+        button.parent().find(".text").toggle();
+      });
+      
     }
   }
 
