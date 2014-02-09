69598c1dd6cd7e3331dc1a530f4e584017315ea1
Sam Pattuzzi <sam.pattuzzi@gmail.com>
2014-02-09 09:36:48 +0000

    Cover the Google mail page with our single. Hidden by default.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,24 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+		setTimeout(function(){
+			hashtags.injectIFrame()
+		}, 1000);
 	}
 	}, 10);
 });
+
+hashtags = {
+	
+	injectIFrame: function() {
+		$("html").append("<iframe id='hashtag-view' src='"+chrome.extension.getURL("ui/single.html")+"' style='height:100%; width:100%; top:0px; right:0px; position:absolute; z-index:1000; display:none;'></iframe>");
+	},
+	
+	showIFrame: function() {
+		$("#hashtag-view").fadeIn();
+	},
+	
+	hideIFrame: function() {
+		$("#hashtag-view").fadeOut();
+	}
+}
