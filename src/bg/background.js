a7ef2918b415e35e4982881bc716e756511c5acc
Sam Pattuzzi <sam.pattuzzi@gmail.com>
2014-02-09 11:24:55 +0000

    Connect page_action to show hiding.

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,15 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	chrome.pageAction.show(sender.tab.id);
+  sendResponse();
+});
+
+
+chrome.pageAction.onClicked.addListener(function(tab){
+  console.log("got show tags.");
+  console.log(tab.id);
+  chrome.tabs.sendMessage(tab.id, {msg_type: "show_tags"}, function(response) {
   });
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,21 +10,39 @@ chrome.extension.sendMessage({}, function(response) {
 		setTimeout(function(){
 			hashtags.injectIFrame()
 		}, 1000);
+		
+		chrome.runtime.onMessage.addListener(
+		  function(request, sender, sendResponse) {
+		    console.log(sender.tab ?
+		                "from a content script:" + sender.tab.url :
+		                "from the extension");
+		    if (request.msg_type == "show_tags") {
+		    	if (hashtags.showing) {
+		    		hashtags.hideIFrame();
+		    	} else {
+					hashtags.showIFrame({});
+		    	}
+		    }
+		  });
 	}
 	}, 10);
 });
 
 hashtags = {
 	
+	showing: false,
+	
 	injectIFrame: function() {
 		$("html").append("<iframe id='hashtag-view' src='"+chrome.extension.getURL("ui/single.html")+"' style='height:100%; width:100%; top:0px; right:0px; position:absolute; z-index:1000; display:none;'></iframe>");
 	},
 	
-	showIFrame: function() {
+	showIFrame: function(messages) {
 		$("#hashtag-view").fadeIn();
+		hashtags.showing = true;
 	},
 	
 	hideIFrame: function() {
 		$("#hashtag-view").fadeOut();
+		hashtags.showing = false;
 	}
 }
