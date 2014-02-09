47452053bf46b9e191f01a21686a208a1b132f1b
Karina Palyutina <karina.palyutina@gmail.com>
2014-02-09 10:40:04 +0000

    Toggle the right way round; email body preview is cut to 100 chars.

--- ui/js/app.js
+++ ui/js/app.js
@@ -16,11 +16,11 @@ $(document).ready(function() {
     
     for (var x = 0; x < hashtags[i].emails.length; x++) {
       var email = emails[x];
-      var message = $('<div class="message panel panel-default">').wrap('</div>').appendTo(messages);
+      var message = $('<div class="message panel panel-success">').wrap('</div>').appendTo(messages);
       var panel_heading = $('<div class="panel-heading">').html("<b>From: </b>" + email.from.email + " </br><b>To: </b>" + email.to.length + puluralise(" recipient", email.to.length)).wrap('</div>').appendTo(message);
       var panel_body = $('<div class="panel-body">').wrap('</div>').appendTo(message);
-      var body1 = $('<div class="text">').text(email.body).wrap('</div>').appendTo(panel_body);
-      var body2 = $('<div class="text" style="display: none;">').text(email.body + "3495873985793").wrap('</div>').appendTo(panel_body);
+      var body1 = $('<div class="text">').text(email.body.substring(0,100)).wrap('</div>').appendTo(panel_body);
+      var body2 = $('<div class="text" style="display: none;">').text(email.body).wrap('</div>').appendTo(panel_body);
       var button = $('<button type="button" style="float:right;" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-chevron-down"></span>').wrap('</button>').appendTo(panel_body);
 
       button.bind("click", function(e) {
